6a6ae2d488fa3b2cb2da8e2437f9d42855f453c8
CEDipEngineering <carlosed3@al.insper.com.br>
2020-04-28 17:45:57 -0300

    Progress in AI scripting, and started implementing AI into proj.py (executable)

--- projeto1-robcomp/scripts/AI.py
+++ projeto1-robcomp/scripts/AI.py
@@ -1,20 +1,74 @@
 import numpy as np
 import cv2
 import time
+from classes import Point, Line
+from geometry_msgs.msg import Twist, Vector3
+from cv_bridge import CvBridge, CvBridgeError
+import visao_module
+import rospy
+
 
 class AI:
 
     def __init__(self):
-        pass
+        self.frame = None
+        self.buffering = 5
+        self.lista_goodLeft = [0]*self.buffering
+        self.lista_goodRight = [0]*self.buffering
+        self.lydar = None
+        self.__acceptableDelay__ = 1.5E9
+        self.cv_bridge = CvBridge() 
+        self.mobileNetResults = None
 
-    def steer(self):
-        pass
-
-    def setFrame(self):
-        pass
+    def alignToTarget(self, point):
+        if self.checkFrame():
+            targetX = point.x
+            currentX = self.frame.shape[0]//2
+            direction = targetX - currentX
+            velArr = [Vector3(0,0,0), Vector3(0,0,0)]
+            if direction >= 3: 
+                velArr = [Vector3(0,0,0), Vector3(0,0,-0.1)]
+            elif direction <= -3:
+                velArr = [Vector3(0,0,0), Vector3(0,0,0.1)]
+            return velArr
     
     def followRoad(self):
-        pass
+        if self.checkFrame():
+            edges = self.__treatForLines__(self.frame.copy())
+            lines = cv2.HoughLines(edges,1,np.pi/180, 75)
+            if lines is not None:
+                for line in lines:
+                    for rho, theta in line:
+                        a = np.cos(theta)
+
+                        b = np.sin(theta)
+                        x0 = a * rho
+                        y0 = b * rho
+                        pt1 = Point(int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+                        pt2 = Point(int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+                        lin = Line(pt1, pt2)
+
+                        if lin.m < -0.2:
+                            self.lista_goodLeft.pop(0)
+                            self.lista_goodLeft.append(lin)
+                        elif lin.m > 0.2:
+                            self.lista_goodRight.pop(0)
+                            self.lista_goodRight.append(lin)
+
+
+                if 0 not in self.lista_goodLeft and 0 not in self.lista_goodRight:
+                    average_Left = self.__calculateMeanLine__(self.lista_goodLeft)
+                    average_Right = self.__calculateMeanLine__(self.lista_goodRight)
+                    a, b = average_Left.getPoints()
+                    c, d = average_Right.getPoints()
+                    # cv2.line(frame, a, b,(255,0,0),2)
+                    # cv2.line(frame, c, d,(255,0,0),2)
+                    inter = average_Left.intersect(average_Right)
+                    cv2.circle(frame, inter, 5,(0,255,255), 5)
+                    return inter
+                else:
+                    return
+
 
     def identifyColor(self):
         pass
@@ -26,15 +80,80 @@ class AI:
         pass
 
     def searchRotate(self):
+        velArr = [Vector3(0,0,0), Vector3(0,0,0.1)]
         pass
 
     def centralizeCamera(self):
         pass
 
     def fastAdvance(self):
-        pass
+        velArr = [Vector3(0.3,0,0), Vector3(0,0,0)]
+        return velArr
 
     def slowAdvance(self):
-        pass
+        velArr = [Vector3(0.1,0,0), Vector3(0,0,0)]
+        return velArr
 
+    def __calculateMeanLine__(self, linhas):
+        first_point_x = int(round(np.mean([linha.point1.x for linha in linhas])))
+        first_point_y = int(round(np.mean([linha.point1.y for linha in linhas])))
+        second_point_x = int(round(np.mean([linha.point2.x for linha in linhas])))
+        second_point_y = int(round(np.mean([linha.point2.y for linha in linhas])))
+        first_point = Point(first_point_x, first_point_y)
+        second_point = Point(second_point_x, second_point_y)
+        return Line(first_point, second_point)
+
+    def __auto_canny__(self, sigma=0.33):
+        if self.checkFrame():
+            # compute the median of the single channel pixel intensities
+            v = np.median(self.frame.copy())
+
+            # apply automatic Canny edge detection using the computed median
+            lower = int(max(0, (1.0 - sigma) * v))
+            upper = int(min(255, (1.0 + sigma) * v))
+            edged = cv2.Canny(self.frame.copy(), lower, upper)
+
+            # return the edged image
+            return edged
     
+    def __treatForLines__(self):
+        if self.checkFrame():
+            # Shape detection using color (cv2.inRange masks are applied over orginal image)
+            mask = cv2.inRange(cv2.GaussianBlur(self.frame.copy(),(5,5),0),np.array([0,0,0]),np.array([10,10,255]))
+            morphMask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,np.ones((6, 6)))
+            contornos, arvore = cv2.findContours(morphMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            frame_out = cv2.drawContours(morphMask, contornos, -1, [0, 0, 255], 3)
+            return frame_out
+
+    def setFrame(self, frame):
+        self.frame = frame
+        return
+    
+    def mobileNet(self):
+
+        if self.checkFrame():
+            now = rospy.get_rostime()
+            imgtime = self.frame.header.stamp
+            lag = now-imgtime # calcula o lag
+            delay = lag.nsecs
+            # print("delay ", "{:.3f}".format(delay/1.0E9))
+            if delay > self.__acceptableDelay__:
+                print("Descartando por causa do delay do frame:", delay)
+                self.mobileNetResults = None
+                return 
+            try:
+                antes = time.clock()
+                cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(self.frame, "bgr8")
+
+                _a_, _b_, resultados =  visao_module.processa(cv_image)        
+                depois = time.clock()
+                self.mobileNetResults = resultados
+                return resultados
+            except CvBridgeError as e:
+                print('ex', e)
+                self.mobileNetResults = None
+                return 
+
+    def checkFrame(self):
+        return self.frame != None
+        
--- projeto1-robcomp/scripts/proj.py
+++ projeto1-robcomp/scripts/proj.py
@@ -4,44 +4,34 @@
 from __future__ import print_function, division
 import rospy
 import numpy as np
-import numpy
 import tf
 import math
 import cv2
 import time
-from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-from numpy import linalg
-from tf import transformations
-from tf import TransformerROS
-import tf2_ros
-from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
+# from tf import transformations
+# from tf import TransformerROS
 from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
 from std_msgs.msg import Header
 from numpy import linalg
-from tf import transformations
-from tf import TransformerROS
 import tf2_ros
-import math
 from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
-from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Image
-from std_msgs.msg import Header
 
+from AI import AI
 import visao_module
 
 
 bridge = CvBridge()
+ai = AI()
 
 cv_image = None
 media = []
 centro = []
 atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
-
+from nav_msgs.msg import Odometrfrom nav_msgs.msg import Odometryy
 
 area = 0.0 # Variavel com a area do maior contorno
 
@@ -49,7 +39,7 @@ area = 0.0 # Variavel com a area do maior contorno
 # Descarta imagens que chegam atrasadas demais
 check_delay = False 
 
-resultados = [] # Criacao de uma variavel global para guardar os resultados vistos
+# Criacao de uma variavel global para guardar os resultados vistos
 
 x = 0
 y = 0
@@ -72,7 +62,7 @@ def recebe(msg):
 		id = marker.id
 		marcador = "ar_marker_" + str(id)
 
-		print(tf_buffer.can_transform(frame, marcador, rospy.Time(0)))
+		# print(tf_buffer.can_transform(frame, marcador, rospy.Time(0)))
 		header = Header(frame_id=marcador)
 		# Procura a transformacao em sistema de coordenadas entre a base do robo e o marcador numero 100
 		# Note que para seu projeto 1 voce nao vai precisar de nada que tem abaixo, a 
@@ -86,68 +76,36 @@ def recebe(msg):
 		# ATENCAO: tudo o que vem a seguir e'  so para calcular um angulo
 		# Para medirmos o angulo entre marcador e robo vamos projetar o eixo Z do marcador (perpendicular) 
 		# no eixo X do robo (que e'  a direcao para a frente)
-		t = transformations.translation_matrix([x, y, z])
+		t = tf.transformations.translation_matrix([x, y, z])
 		# Encontra as rotacoes e cria uma matriz de rotacao a partir dos quaternions
-		r = transformations.quaternion_matrix([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
-		m = numpy.dot(r,t) # Criamos a matriz composta por translacoes e rotacoes
+		r = tf.transformations.quaternion_matrix([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
+		m = np.dot(r,t) # Criamos a matriz composta por translacoes e rotacoes
 		z_marker = [0,0,1,0] # Sao 4 coordenadas porque e'  um vetor em coordenadas homogeneas
-		v2 = numpy.dot(m, z_marker)
+		v2 = np.dot(m, z_marker)
 		v2_n = v2[0:-1] # Descartamos a ultima posicao
 		n2 = v2_n/linalg.norm(v2_n) # Normalizamos o vetor
 		x_robo = [1,0,0]
-		cosa = numpy.dot(n2, x_robo) # Projecao do vetor normal ao marcador no x do robo
+		cosa = np.dot(n2, x_robo) # Projecao do vetor normal ao marcador no x do robo
 		angulo_marcador_robo = math.degrees(math.acos(cosa))
 
 		# Terminamos
-		print("id: {} x {} y {} z {} angulo {} ".format(id, x,y,z, angulo_marcador_robo))
+		# print("id: {} x {} y {} z {} angulo {} ".format(id, x,y,z, angulo_marcador_robo))
 
 
-
-# A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
-    print("frame")
-    global cv_image
-    global media
-    global centro
-
-    global resultados
+    ai.setFrame(imagem)
 
 
-    now = rospy.get_rostime()
-    imgtime = imagem.header.stamp
-    lag = now-imgtime # calcula o lag
-    delay = lag.nsecs
-    # print("delay ", "{:.3f}".format(delay/1.0E9))
-    if delay > atraso and check_delay==True:
-        print("Descartando por causa do delay do frame:", delay)
-        return 
-    try:
-        antes = time.clock()
-        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-        # Note que os resultados já são guardados automaticamente na variável
-        # chamada resultados
-        centro, imagem, resultados =  visao_module.processa(cv_image)        
-        for r in resultados:
-            # print(r) - print feito para documentar e entender
-            # o resultado            
-            pass
 
-        depois = time.clock()
-        # Desnecessário - Hough e MobileNet já abrem janelas
-        #cv2.imshow("Camera", cv_image)
-    except CvBridgeError as e:
-        print('ex', e)
     
 if __name__=="__main__":
+
     rospy.init_node("cor")
-
     topico_imagem = "/camera/rgb/image_raw/compressed"
-
+    topico_imagem2 = "/raspicam/rgb/image_raw/compressed"
     recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
     recebedor = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, recebe) # Para recebermos notificacoes de que marcadores foram vistos
-
-
-    print("Usando ", topico_imagem)
+    # print("Usando ", topico_imagem)
 
     velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
@@ -159,12 +117,14 @@ if __name__=="__main__":
     # [(categoria, probabilidade (certeza), canto sup.esq do retângulo, canto inf.dir do retângulo)]
 
     try:
-        # Inicializando - por default gira no sentido anti-horário
-        # vel = Twist(Vector3(0,0,0), Vector3(0,0,math.pi/10.0))
-        
+       
         while not rospy.is_shutdown():
-            for r in resultados:
-                print(r)
+            if ai.checkFrame():
+                ai.mobileNet()
+                resultados = ai.mobileNetResults
+                for r in resultados:
+                    print(r)
+                
             #velocidade_saida.publish(vel)
             rospy.sleep(0.1)
 
